682f09720dbcfe31bcc8020d14c265537626697a
Shawn Greaney <onedementedsmurf@gmail.com>
2025-06-02 21:09:06 -0400

    Add notes, comment out server specific hard reloading.

--- bot.js
+++ bot.js
@@ -27,20 +27,22 @@ for (const file of registrationFiles) {
     }
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-const GUILD_ID = process.env.GUILD_ID;
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, GUILD_ID),
-            { body: allCommands },
-        );
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+
+// Register commands with Discord, hard reload for specific server.
+// const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+// const GUILD_ID = process.env.GUILD_ID;
+// (async () => {
+//     try {
+//         console.log('Started refreshing application (/) commands.');
+//         await rest.put(
+//             Routes.applicationGuildCommands(process.env.CLIENT_ID, GUILD_ID),
+//             { body: allCommands },
+//         );
+//         console.log('Successfully reloaded application (/) commands.');
+//     } catch (error) {
+//         console.error(error);
+//     }
+// })();
 
 // SINGLE interactionCreate HANDLER
 client.on('interactionCreate', async interaction => {
